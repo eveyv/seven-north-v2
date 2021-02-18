bb0e86c8db54b8e6d2fdbbc4956cb1b0f9cce8d9
Everett Yeaw <50276198+eveyv@users.noreply.github.com>
2021-02-17 20:39:51 -0500

    Update Subscribe.js

--- src/components/Subscribe.js
+++ src/components/Subscribe.js
@@ -6,7 +6,7 @@ export default function Subscribe() {
       <div className="container">
         <div className="row">
           <div className="col-md-10 col-lg-8 mx-auto text-center">
-            <i className="far fa-heart fa-4x mb-2 text-white"></i>
+            <i className="fa-comments fa-4x mb-2 text-white"></i>
             <h2 className="text-white mb-5">We would love to get in touch!</h2>
           </div>
         </div>
