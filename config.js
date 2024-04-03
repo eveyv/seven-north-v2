5216aebef94541268e1d2690c6e964a7b85e1b76
Everett Yeaw <50276198+eveyv@users.noreply.github.com>
2024-04-03 14:17:13 +0000

    testing gh-pages

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/seven-north-v2`, // This path is subpath of your hosting https://domain/portfolio
+  pathPrefix: `/seven-north-v2/`, // This path is subpath of your hosting https://domain/portfolio
   heading: 'Seven North',
   subHeading: 'Website Design',
 
